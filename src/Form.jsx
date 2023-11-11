30d06875d98155272b4792ad9eba29ee7cc029f1
fissid <fissid.ss@gmail.com>
2023-11-11 17:10:32 +0300

    updates

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,19 +1,32 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [desc, setDesc] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function addHandler(e) {
     e.preventDefault();
-    console.log(e.target);
+
+    if (!desc) return;
+
+    const newItem = {
+      description: desc,
+      quantity: quantity,
+      packed: false,
+      id: Math.floor(Math.random() * 100),
+    };
+    console.log(newItem);
   }
   return (
     <form className="add-form" onSubmit={addHandler}>
       <h3>What do you need for your trip? 📃</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (cur, i) => i + 1).map((each) => (
           <option key={each} value={each}>
             {each}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="item" />
+      <input type="text" placeholder="Item..." value={desc} onChange={(e) => setDesc(e.target.value)} />
       <button>Add</button>
     </form>
   );
