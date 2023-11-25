86069e2fd1d8bd4dc7b0df98c8e2a26a89ee6d64
fissid <fissid.ss@gmail.com>
2023-11-25 23:19:47 +0300

    updates

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -2,7 +2,7 @@ export default function Stats({ length, packed }) {
   return (
     <footer className="stats">
       <em>
-        You have {length} items on your list, and you already packed ({(length / packed) * 10}%) of them.{" "}
+        You have {length} items on your list, and you already packed ({packed === 0 ? 0 : Math.floor((packed / length) * 100)}%) of them.{" "}
       </em>
     </footer>
   );
