3a3e524224f2120f97c430c5718b98c19653e54c
fissid <fissid.ss@gmail.com>
2023-11-25 23:33:20 +0300

    updates

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -2,7 +2,7 @@ export default function Stats({ length, packed }) {
   const percentage = Math.floor((packed / length) * 100);
   return (
     <footer className="stats">
-      <em>{percentage !== 100 ? `You have ${length} items on your list, and you already packed (${packed === 0 ? 0 : percentage}%) of them. 👌` : `You got everything! Ready to go 🚀`}</em>
+      <em>{percentage !== 100 ? `You have ${length} items on your list, and you already packed (${packed === 0 ? 0 : percentage}%) of them 👌` : `You got everything! Ready to go 🚀`}</em>
     </footer>
   );
 }
